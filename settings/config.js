242dc2d8a7bfac19d5999642455af11ce6909bc2
KarishmaBothara <bothara.karishma@gmail.com>
2021-04-14 10:48:55 +1200

    change the endpoint to Nikau

--- settings/config.js
+++ settings/config.js
@@ -1,7 +1,7 @@
 if (typeof window !== 'undefined') {
     window.config = {
-        ENDPOINT: 'ws://127.0.0.1:9944/',
-        // ENDPOINT: 'wss://nikau.centrality.me/public/ws',
+        // ENDPOINT: 'ws://127.0.0.1:9944/',
+        ENDPOINT: 'wss://nikau.centrality.me/public/ws',
         ENV: 'local',
         FEE_BUFFER: 0.05,
         MAX_FEE_BUFFER: 0.5,
