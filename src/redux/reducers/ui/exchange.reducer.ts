f1b13674c822b69ad505e3fe2ddd757ed35dd9ca
KarishmaBothara <bothara.karishma@gmail.com>
2021-05-18 16:49:53 +1200

    Few fixes around remove liquidity and reset (liquidity/exchange) page

--- src/redux/reducers/ui/exchange.reducer.ts
+++ src/redux/reducers/ui/exchange.reducer.ts
@@ -80,7 +80,16 @@ export default handleActions<ExchangeState, any>(
         [ExchangeActions.EXCHANGE_RATE_UPDATE]: produce((draft: ExchangeState, action: UpdateExchangeRateAction) => {
             draft.exchangeRate = action.payload;
         }),
-        [ExchangeActions.TRADE_RESET]: () => initialState,
+        [ExchangeActions.TRADE_RESET]: produce((draft: ExchangeState) => {
+            draft.form.fromAssetAmount = undefined;
+            draft.form.toAssetAmount = undefined;
+            draft.form.extrinsic = undefined;
+            draft.form.fromAsset = undefined;
+            draft.form.toAsset = undefined;
+            draft.form.signingAccount = undefined;
+            draft.userAssetBalance = [];
+            draft.error = [];
+        }),
         [ExchangeActions.ASSET_SWAP]: produce((draft: ExchangeState) => {
             const draftFromAsset = draft.form.fromAsset;
             draft.form.fromAsset = draft.form.toAsset;
