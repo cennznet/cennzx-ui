c89cc4aeb7f2f98749b140e5894fe96747e7782f
Eugene Upston <eupston130@hotmail.com>
2021-08-10 17:39:17 -0600

    Updates

---
+++ settings/config.js
@@ -0,0 +1,21 @@
+let ENDPOINT = '', ENV = '';
+if (typeof window !== 'undefined') {
+    const hostname = window.location.hostname;
+    if (hostname.endsWith('centrality.me')) {
+        ENDPOINT = 'wss://nikau.centrality.me/public/ws';
+        ENV = 'DEV';
+    } else if (hostname.endsWith('cennzx.io')) {
+        ENDPOINT = 'wss://cennznet.unfrastructure.io/public/ws';
+        ENV = 'PROD';
+    } else {
+        ENDPOINT = 'ws://127.0.0.1:9944/'
+        ENV = 'local';
+    }
+    window.config = {
+        ENDPOINT: ENDPOINT,
+        ENV: ENV,
+        FEE_BUFFER: 0.05,
+        MAX_FEE_BUFFER: 0.5,
+        MIN_FEE_BUFFER: 0.01
+    }
+}
